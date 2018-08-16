7a5a2b2222093e3d8c6a47bb6f359bf3c5a8f626
Chris <chrisdavidramos@gmail.com>
2018-08-16 13:43:18 -0400

    bump

--- Code/Src/UtilitiesHandler.js
+++ Code/Src/UtilitiesHandler.js
@@ -12,13 +12,13 @@ function formatTimeService(date) {
 
 function timeConversion(millisec) {
     // https://stackoverflow.com/a/32180863
-    var seconds = (millisec / 1000).toFixed(1);
+    var seconds = Math.round((millisec / 1000).toFixed(1));
 
-    var minutes = (millisec / (1000 * 60)).toFixed(1);
+    var minutes = Math.round((millisec / (1000 * 60)).toFixed(1));
 
-    var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+    var hours = Math.round((millisec / (1000 * 60 * 60)).toFixed(1));
 
-    var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+    var days = Math.round((millisec / (1000 * 60 * 60 * 24)).toFixed(1));
 
     if (seconds < 60) {
         return seconds + " Sec";
