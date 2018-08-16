d85e9ac287b94853669dccb107eb67544a9e6399
Chris <chrisdavidramos@gmail.com>
2018-08-16 11:58:57 -0400

    bump

--- Code/Src/MessageHandler.js
+++ Code/Src/MessageHandler.js
@@ -4,7 +4,7 @@ function MessageData(message) {
     var strEmail = message.getFrom();
     var contacts = ContactsApp.getContactsByEmailAddress(strEmail);
 
-
+    Logger.log(contacts[0]);
     this.sender = contacts[0];
     this.subject = message.getSubject();
     this.date = message.getDate();
