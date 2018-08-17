cacc046e685b30f29957b2905566e4f689e893fb
Chris <chrisdavidramos@gmail.com>
2018-08-17 00:11:54 -0400

    bump

--- Code/Src/UniversalActions.ts
+++ Code/Src/UniversalActions.ts
@@ -1,7 +1,13 @@
 
 function openLinkUniversalAction() {
+    var cardBuilder1 = CardService.newCardBuilder();
+    var cardBuilder2 = CardService.newCardBuilder();
+    // Finish building the cards ...
+
+    // A universal action that shows two static cards.
     return CardService.newUniversalActionResponseBuilder()
-        .setOpenLink(CardService.newOpenLink()
-                     .setUrl("https://www.google.com"))
-        .build();
+        .displayAddOnCards([
+            cardBuilder1.build();
+            cardBuilder2.build();
+        ]).build();
 }
