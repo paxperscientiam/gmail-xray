3ff29713a2637bd323677f66e2db6418946b1234
Chris <chrisdavidramos@gmail.com>
2018-08-18 20:43:33 -0400

    bump

--- Code/Src/ThreadHandler.ts
+++ Code/Src/ThreadHandler.ts
@@ -1,10 +1,12 @@
 function ThreadData(thread) {
+    var sender = extractEmailAddress(this.message.getFrom());
+
     this.messages = thread.getMessages();
     this.message = this.messages[0];
     this.labels = thread.getLabels();
     this.count = thread.getMessageCount();
     this.link = thread.getPermalink();
     //
-    this.sender = this.message.getFrom();
+    this.sender = sender;
     this.subject = this.message.getSubject();
 }
