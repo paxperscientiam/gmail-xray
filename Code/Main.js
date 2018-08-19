5e9d28aebe058a789cf37a0299d771af8f6b35b3
Chris <chrisdavidramos@gmail.com>
2018-08-19 16:38:06 -0400

    bump

--- Code/Main.js
+++ Code/Main.js
@@ -5,42 +5,48 @@ function buildAddOn(e) {
     // GmailApp.setCurrentMessageAccessToken(accessToken);
     // var messageId = e.messageMetadata.messageId;
     // var senderData = extractSenderData(messageId);
-    var accessToken = e.messageMetadata.accessToken;
-    Logger.log(accessToken);
     var MAILBOX_QUERY = props.getProperty("MAILBOX_QUERY");
     var MAX_THREADS = props.getProperty("MAX_THREADS");
 
     var threads = GmailApp.search(MAILBOX_QUERY, 0, MAX_THREADS);
     var cards = [];
 
-    var threadData = {};
+    var cardTop = CardService.newCardBuilder()
+        .setName("Card name")
+        .setHeader(CardService.newCardHeader().setTitle("Card title"))
+        .addSection(cardSection)
+        .build();
 
-    for (var i = 0; i < threads.length && i < MAX_THREADS; i++) {
-        threadData = new ThreadData(threads[i]); // a thread from set of threads
-        var message = threadData.message[0]; // a message from set of messages in a thread
+    cards.push(cardTop);
 
-        var threadLength = threadData.threadLength;
+  //   var threadData = {};
 
-        var card = CardService.newCardBuilder()
-            .setHeader(( new CardHeader(threadData) ));
+//     for (var i = 0; i < threads.length && i < MAX_THREADS; i++) {
+//         threadData = new ThreadData(threads[i]); // a thread from set of threads
+//         var message = threadData.message[0]; // a message from set of messages in a thread
 
-        for (var j = 0; j < threadLength; j++) {
-            var Obj = mergeObjs({index: j}, threadData, message);
-            var msgSection = new CardSection(Obj).setCollapsible(false);
-            var actionSection = new CardSectionActionCenter();
-            card = SectionChainer(card, [msgSection, actionSection]);
-        }
+//         var threadLength = threadData.threadLength;
 
-        // this is causing errors!
-  //      var foot = new CardSectionSecondary();
-    //    foot.setCollapsible(true);
-//        card = SectionChainer(card, {msg: foot});
+//         var card = CardService.newCardBuilder()
+//             .setHeader(( new CardHeader(threadData) ));
 
-        cards.push(card
-                   .setName("Card name")
-                   .build());
+//         for (var j = 0; j < threadLength; j++) {
+//             var Obj = mergeObjs({index: j}, threadData, message);
+//             var msgSection = new CardSection(Obj).setCollapsible(false);
+//             var actionSection = new CardSectionActionCenter();
+//             card = SectionChainer(card, [msgSection, actionSection]);
+//         }
 
-    }
+//         // this is causing errors!
+//   //      var foot = new CardSectionSecondary();
+//     //    foot.setCollapsible(true);
+// //        card = SectionChainer(card, {msg: foot});
+
+//         cards.push(card
+//                    .setName("Card name")
+//                    .build());
+
+//     }
     return cards;
 }
 
