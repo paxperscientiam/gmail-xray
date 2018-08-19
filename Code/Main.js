7355291f065e20d16df7d67917406116c16d87f9
Chris <chrisdavidramos@gmail.com>
2018-08-18 20:18:25 -0400

    bump

--- Code/Main.js
+++ Code/Main.js
@@ -80,12 +80,12 @@ function CardSectionActionCenter() {
     return this.section;
 }
 
-function buildAddOn() {
+function buildAddOn(e) {
     // Activate temporary Gmail add-on scopes.
-    // var accessToken = e.messageMetadata.accessToken;
-    // GmailApp.setCurrentMessageAccessToken(accessToken);
-    // var messageId = e.messageMetadata.messageId;
-    // var senderData = extractSenderData(messageId);
+    var accessToken = e.messageMetadata.accessToken;
+    GmailApp.setCurrentMessageAccessToken(accessToken);
+    var messageId = e.messageMetadata.messageId;
+    var senderData = extractSenderData(messageId);
 
     var MAILBOX_QUERY = props.getProperty("MAILBOX_QUERY");
     var MAX_THREADS = props.getProperty("MAX_THREADS");
