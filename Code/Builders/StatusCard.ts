9d6f48212df8839ee2b267177d74f4f7615269d9
Chris <chrisdavidramos@gmail.com>
2018-08-20 17:15:49 -0400

    bump

--- Code/Builders/StatusCard.ts
+++ Code/Builders/StatusCard.ts
@@ -4,11 +4,19 @@ function StatusCard() {
         .setHeader("section one")
         .addWidget(WidgetHandler("balls"));
 
+    var calData =  new CalendarHandler();
+
+    var sectionTwo = CardService.newCardSection()
+        .setHeader("What's up?")
+        .addWidget(WidgetHandler(calData.name));
+
     return CardService.newCardBuilder()
         .setName("status card")
         .setHeader(CardService.newCardHeader()
                    .setTitle("STATUS CENTER")
                    .setSubtitle(dt.GREETING + ", today is " + dt.WEEKDAY))
         .addSection(sectionOne)
+        .addSection(sectionTwo)
         .build();
+
 }
--- Code/CalendarStuff.ts
+++ Code/CalendarStuff.ts
@@ -1,24 +1,9 @@
-function listUpcomingEvents() {
-  var calendarId = 'primary';
-  var optionalArgs = {
-    timeMin: (new Date()).toISOString(),
-    showDeleted: false,
-    singleEvents: true,
-    maxResults: 10,
-    orderBy: 'startTime'
-  };
-  var response = Calendar.Events.list(calendarId, optionalArgs);
-  var events = response.items;
-  if (events.length > 0) {
-    for (i = 0; i < events.length; i++) {
-      var event = events[i];
-      var when = event.start.dateTime;
-      if (!when) {
-        when = event.start.date;
-      }
-      Logger.log('%s (%s)', event.summary, when);
-    }
-  } else {
-    Logger.log('No upcoming events found.');
-  }
+function CalendarHandler() {
+    var today = new Date();
+
+    this.calendar = CalendarApp.getDefaultCalendar();
+    //
+    this.name = this.calendar.getName();
+    this.eventsToday = this.calendar.getEventsForDay(today);
+
 }
