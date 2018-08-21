c8f03ff6cbdc2a7b266b053660fffa446660763d
Chris <chrisdavidramos@gmail.com>
2018-08-20 23:11:52 -0400

    bump

--- Code/Widgets/WeatherWidget.ts
+++ Code/Widgets/WeatherWidget.ts
@@ -2,11 +2,17 @@ function WeatherWidget() {
   //   var query = '';
     var url = "https://api.weather.gov/stations/KMKC/observations/current";
   // + '?sort=stars'
-  // + '&q=' + encodeURIComponent(query);
+    // + '&q=' + encodeURIComponent(query);
+    var params = {
+        headers: {
+            "Accept": "application/vnd.noaa.dwml+xml;version=1",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0",
+        },
+        muteHttpExceptions: true,
+    };
 
-    var response = UrlFetchApp.fetch(url, {muteHttpExceptions: true});
+    var response = UrlFetchApp.fetch(url, headers);
     Logger.log(response.getContentText());
-
 
     var response = UrlFetchApp.fetch();
     Logger.log(response.properties.presentWeather[0].weather);
