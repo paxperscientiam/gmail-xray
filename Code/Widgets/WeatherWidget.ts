86fca3db030f014e2deddcca3d2b6f95c61c3ede
Chris <chrisdavidramos@gmail.com>
2018-08-21 13:17:14 -0400

    bump

--- Code/Widgets/WeatherWidget.ts
+++ Code/Widgets/WeatherWidget.ts
@@ -1,21 +1,21 @@
 function WeatherWidget() {
-  // //   var query = '';
-  //   var url = "https://api.weather.gov/stations/KMKC/observations/current";
-  // // + '?sort=stars'
-  //   // + '&q=' + encodeURIComponent(query);
-  //   var params = {
-  //       headers: {
-  //           "Accept": "application/vnd.noaa.dwml+xml;version=1",
-  //           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0",
-  //       },
-  //       muteHttpExceptions: true,
-  //   };
+    //   var query = '';
+    var url = "https://api.weather.gov/stations/KMKC/observations/current";
+    // + '?sort=stars'
+    // + '&q=' + encodeURIComponent(query);
+    var params = {
+        headers: {
+            "Accept": "application/vnd.noaa.dwml+xml;version=1",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0",
+        },
+        muteHttpExceptions: true,
+    };
 
-  //   var response = UrlFetchApp.fetch(url, params);
-  //   var json = response.getContentText();
-  //   var data = JSON.parse(json);
+    var response = UrlFetchApp.fetch(url, params);
+    var json = response.getContentText();
+    var data = JSON.parse(json);
 
-  //   var wx = data.properties.presentWeather[0].weather;
+    var wx = data.properties.presentWeather[0].weather;
 
-    return CardService.newTextParagraph().setText("Stay tuned!");
+    return CardService.newTextParagraph().setText(wx);
 }
