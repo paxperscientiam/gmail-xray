189943af1626aa7414cc4176b18584ae04612a5d
Chris <chrisdavidramos@gmail.com>
2018-08-21 12:14:46 -0400

    bump

--- Code/Handlers/ThreadHandler.ts
+++ Code/Handlers/ThreadHandler.ts
@@ -2,7 +2,7 @@ function ThreadData(thread) {
     // thread
 
     this.messages = thread.getMessages();
-    this.message = this.messages[0];
+    this.firstMessage = this.messages[0];
     this.labels = thread.getLabels();
     this.count = thread.getMessageCount();
     this.link = thread.getPermalink();
