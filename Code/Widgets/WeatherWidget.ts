0a33895374d06de235ab14b0e7c86e04b490b661
Chris <chrisdavidramos@gmail.com>
2018-08-20 22:43:47 -0400

    bump

--- Code/Widgets/WeatherWidget.ts
+++ Code/Widgets/WeatherWidget.ts
@@ -1,4 +1,7 @@
 function WeatherWidget() {
+    var response = UrlFetchApp.fetch("https://api.weather.gov/stations/KMKC/observations/current");
+    Logger.log(response.properties.presentWeather[0].weather);
+
     return CardService.newTextParagraph().setText(
         "These widgets are display-only. " +
             'A text paragraph can have multiple lines and ' +
