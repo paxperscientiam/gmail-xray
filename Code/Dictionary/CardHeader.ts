4befbb86a7f09d00010669e94b53ca2cafe0cf83
Chris <chrisdavidramos@gmail.com>
2018-08-21 23:22:18 -0400

    bump

--- Code/Dictionary/CardHeader.ts
+++ Code/Dictionary/CardHeader.ts
@@ -2,7 +2,7 @@ function CardHeader(message) {
     const messageData = new MessageData(message);
     var imgUrl;
 
-    if (message.starred) {
+    if (message.starred === true) {
         imgUrl = "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/material/star/ic_star_rate_black_18dp_1x.png";
     } else {
         imgUrl = "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/icons8-secured-letter-64.png";
