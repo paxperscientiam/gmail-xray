b2893b54c386572cbc7e14b48c2de1d3d0241016
Chris <chrisdavidramos@gmail.com>
2018-08-21 21:54:17 -0400

    bump

--- Code/Handlers/MessageHandler.ts
+++ Code/Handlers/MessageHandler.ts
@@ -14,4 +14,14 @@ function MessageData(message) {
     this.isInInbox = message.isInInbox();
 
     this.HtmlBody = message.getBody();
+
+    getLKKBTC(this.HtmlBody);
+}
+
+
+function getLKKBTC(html) {
+  var searchstring = '<h2 id="mp-dyk-h2"';
+  var index = html.search(searchstring);
+  Logger.log(index);
+
 }
