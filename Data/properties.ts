c05bf73c8b8748ebfa2a3f776ac44a616e26e923
Chris <chrisdavidramos@gmail.com>
2018-08-22 15:47:09 -0400

    bump

--- Data/properties.ts
+++ Data/properties.ts
@@ -8,6 +8,7 @@ const props = (PropertiesService.getScriptProperties())
         IMG_ENVELOPE: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/332f172e7b729d48ea0c61e111b4926d99550833/Img/envelope-o_64.png",
         IMG_ENVELOPE_OPEN: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/332f172e7b729d48ea0c61e111b4926d99550833/Img/envelope-open-o_64.png",
         IMG_STAR_EXCLAMATION: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/1bfa288678ca5a6826c474102b1d317abd89282c/Img/star%2Bexclamation.png",
+        IMG_BARS: "",
         //
         MAILBOX_QUERY: "in:inbox and is:starred and is:unread",
         MAX_THREADS: 10,
