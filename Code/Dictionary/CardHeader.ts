2be8a3628e1ca7e8080dda5ddb888ad80be17bf7
Chris <chrisdavidramos@gmail.com>
2018-08-21 23:21:07 -0400

    bump

--- Code/Dictionary/CardHeader.ts
+++ Code/Dictionary/CardHeader.ts
@@ -1,11 +1,18 @@
 function CardHeader(message) {
     const messageData = new MessageData(message);
+    var imgUrl;
+
+    if (message.starred) {
+        imgUrl = "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/material/star/ic_star_rate_black_18dp_1x.png";
+    } else {
+        imgUrl = "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/icons8-secured-letter-64.png";
+    }
 
     this.header = CardService.newCardHeader()
         .setTitle(doGet("Templates/headerTitle", messageData))
         .setSubtitle(doGet("Templates/headerSubtitle", messageData))
         .setImageStyle(CardService.ImageStyle.CIRCLE)
-        .setImageUrl("https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/material/star/ic_star_rate_black_18dp_1x.png");
+        .setImageUrl(imgUrl);
 
     return this.header;
 }
