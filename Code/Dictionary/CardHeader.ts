fc8c5ad15c4d1b2bccce3a9e85272c3912b31184
Chris <chrisdavidramos@gmail.com>
2018-08-21 23:17:30 -0400

    bump

--- Code/Dictionary/CardHeader.ts
+++ Code/Dictionary/CardHeader.ts
@@ -3,6 +3,9 @@ function CardHeader(message) {
 
     this.header = CardService.newCardHeader()
         .setTitle(doGet("Templates/headerTitle", messageData))
-        .setSubtitle(doGet("Templates/headerSubtitle", messageData));
+        .setSubtitle(doGet("Templates/headerSubtitle", messageData))
+        .setImageStyle(CardService.ImageStyle.CIRCLE)
+        .setImageUrl("https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/material/star/ic_star_rate_black_18dp_1x.png");
+
     return this.header;
 }
