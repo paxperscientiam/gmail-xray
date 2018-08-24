a389953227da604f7cf1fba8cb5a32c3a51ce610
Chris <chrisdavidramos@gmail.com>
2018-08-24 01:45:36 -0400

    bump

--- Code/Dictionary/CardSection.ts
+++ Code/Dictionary/CardSection.ts
@@ -41,7 +41,8 @@ function CardSectionActionCenter() {
     const buttonSet = CardService.newButtonSet()
         .addButton(CardService.newTextButton()
                    .setText("Reply")
-                   .setComposeAction(composeAction, CardService.ComposedEmailType.STANDALONE_DRAFT))
+                   .setOnClickOpenLinkAction(action))
+//                   .setComposeAction(composeAction, CardService.ComposedEmailType.STANDALONE_DRAFT))
         .addButton(CardService.newTextButton()
                    .setText("Reply all")
                    .setOnClickOpenLinkAction(action));
