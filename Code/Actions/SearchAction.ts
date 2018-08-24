37ee6a12a8ff0533046d058210310c13a33917d5
Chris <chrisdavidramos@gmail.com>
2018-08-23 23:37:37 -0400

    bump

--- Code/Actions/SearchAction.ts
+++ Code/Actions/SearchAction.ts
@@ -7,6 +7,13 @@ function MailSearchAction() {
 
 function MailSearchButtonHandler(e) {
     // GmailApp.search(
-    //var QUERY = JSON.parse(e.formInput.INPUT_MAIL_SEARCH_KEY);
-    Logger.log(JSON.stringify(e.formInput, null, "  "));
+    const MAILBOX_QUERY = JSON.stringify(e.formInput.INPUT_MAIL_SEARCH_KEY, null, "  ");
+
+    var MAILBOX_QUERY = props.getProperty("MAILBOX_QUERY");
+    var MAX_THREADS = props.getProperty("MAX_THREADS");
+
+    var threads = GmailApp.search(MAILBOX_QUERY, 0, MAX_THREADS);
+
+    Logger.log(threads.length);
+
 }
