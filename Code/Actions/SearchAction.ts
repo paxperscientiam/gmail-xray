1363cead72c409a4085fcfe832a0ffdf64bf6b48
Chris <chrisdavidramos@gmail.com>
2018-08-23 22:38:46 -0400

    bump

--- Code/Actions/SearchAction.ts
+++ Code/Actions/SearchAction.ts
@@ -7,6 +7,6 @@ function MailSearchAction() {
 
 function MailSearchButtonHandler(e) {
     // GmailApp.search(
-    var QUERY = JSON.parse(e.formInput.INPUT_MAIL_SEARCH_KEY);
+    //var QUERY = JSON.parse(e.formInput.INPUT_MAIL_SEARCH_KEY);
     Logger.log(JSON.stringify(e.formInput, null, "  "));
 }
