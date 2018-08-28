85b189cee64ec0ded711512bcf790bc7841a589d
Chris <chrisdavidramos@gmail.com>
2018-08-28 13:25:05 -0400

    bump

--- Code/Handlers/Stats.ts
+++ Code/Handlers/Stats.ts
@@ -1,10 +1,15 @@
 function StatsHandler(threads) {
     const length = [];
+    const sender = [];
 
     threads.forEach((thread) => {
         const rez = new ThreadData(thread);
         length.push(rez.count);
+        sender.push(rez.sender);
     });
 
     this.length = mean(length);
+
+    Logger.log(compressArray(sender));
+
 }
