a5982605ab5791bf0d75d12293368e2afa08627b
Chris <chrisdavidramos@gmail.com>
2018-09-01 16:35:56 -0400

    bump

--- Data/properties.ts
+++ Data/properties.ts
@@ -11,7 +11,8 @@ const props = (PropertiesService.getScriptProperties())
         IMG_BARS: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/bars_64.png",
         IMG_COMPOSE_NEW: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/plus-circle-64.png",
         IMG_REPLY_THIS: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/reply-64.png",
-        IMG_REPLY_THAT: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/reply-64.png",
+        IMG_REPLY_THAT: "https://raw.githubusercontent.com/paxperscientiam/gmail-xray/master/Img/long-arrow-left-64.png",
+
         //
         MAILBOX_QUERY: "in:inbox and is:starred",
         MAX_THREADS: 20,
