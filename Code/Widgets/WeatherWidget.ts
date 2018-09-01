316906fc23fa1d00e70bd61918c50b0b56d2f381
Chris <chrisdavidramos@gmail.com>
2018-09-01 17:09:29 -0400

    bump

--- Code/Widgets/WeatherWidget.ts
+++ Code/Widgets/WeatherWidget.ts
@@ -1,26 +1,37 @@
 function WeatherWidget() {
     //   var query = '';
-    const url = "https://api.weather.gov/stations/KMKC/observations/current";
+    //const url = "https://api.weather.gov/stations/KMKC/observations/current";
     // + '?sort=stars'
     // + '&q=' + encodeURIComponent(query);
- //    const params = {
-//         headers: {
-//             "Accept": "application/vnd.noaa.dwml+xml;version=1",
-//             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0",
-//         },
-//         muteHttpExceptions: true,
-//     };
+    //    const params = {
+    //         headers: {
+    //             "Accept": "application/vnd.noaa.dwml+xml;version=1",
+    //             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0",
+    //         },
+    //         muteHttpExceptions: true,
+    //     };
 
-//     try {
-//         const response = UrlFetchApp.fetch(url, params);
-//         const json = response.getContentText();
-//         const data = JSON.parse(json);
+    const params = {
+        muteHttpExceptions: true,
+    };
 
-//         const wx = Math.round(data.properties.temperature.value);
-//         return CardService.newTextParagraph().setText(wx);
-//     } catch (e) {
-//         return CardService.newTextParagraph().setText("Check back later :(");
-    //     }
-    return CardService.newTextParagraph().setText("Check back later :(");
+
+    const url = "http://api.ipstack.com/check?access_key=b2e5ad187eeafde9d9e1de4e777091da"
+
+    try {
+        const response = UrlFetchApp.fetch(url, params);
+        const json = response.getContentText();
+        const data = JSON.parse(json);
+
+       // const wx = Math.round(data.properties.temperature.value);
+        // return CardService.newTextParagraph().setText(wx);
+        const ip = data.ip;
+        return CardService.newTextParagraph().setText(ip);
+
+    } catch (e) {
+        return CardService.newTextParagraph().setText("Check back later :(");
+    }
+
+    //    return CardService.newTextParagraph().setText("Check back later :(");
 
 }
