1dddf7f44ba5a7fb6aa279f34e88e4a48a1b50bf
Chris <chrisdavidramos@gmail.com>
2018-09-02 15:08:23 -0400

    bump

--- Code/Handlers/JsonResponse.ts
+++ Code/Handlers/JsonResponse.ts
@@ -1,22 +1,25 @@
 function JsonResponseHandler(url: string, query?: object, params?: object) {
 
-    if (!params) {
-        params = {
-            muteHttpExceptions: true,
-        };
+    if (params) {
+        params = mergeObjs({muteHttpExceptions: true}, params);
+    } else {
+        params = {muteHttpExceptions: true};
     }
 
     const strQuery = "";
 
     if (query) {
         strQuery = Object.keys(query).map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(query[key])).join("&");
+        url = url + strQuery;
     }
 
-    const uri = url + strQuery;
-    Logger.log(uri);
-
-    const response = UrlFetchApp.fetch(uri, params);
+    try {
+        const response = UrlFetchApp.fetch(uri, params);
     //
-    this.json = response.getContentText();
-    this.data = JSON.parse(this.json);
+        this.json = response.getContentText();
+        this.data = JSON.parse(this.json);
+    } catch (e) {
+        Logger.log(e);
+        this.data = {};
+    }
 }
