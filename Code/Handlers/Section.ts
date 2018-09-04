47b84dcc46e5216c967b38cb534cc8a12ee9b170
Chris <chrisdavidramos@gmail.com>
2018-09-03 21:29:09 -0400

    bump

--- Code/Handlers/Section.ts
+++ Code/Handlers/Section.ts
@@ -1,5 +1,8 @@
 function Section(args) {
+    const textParagraph = CardService.newTextParagraph().setText("shit");
+    //
     this.section = CardService.newCardSection()
-        .setHeader(args.header);
+        .setHeader(args.header)
+        .addWidget(textParagraph);
     return this.section;
 }
