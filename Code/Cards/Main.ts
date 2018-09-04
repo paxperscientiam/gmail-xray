71407aa29709942687ea5ce55ec85e63bd077d0c
Chris <chrisdavidramos@gmail.com>
2018-09-04 00:16:52 -0400

    bump

--- Code/Cards/Main.ts
+++ Code/Cards/Main.ts
@@ -1,12 +1,12 @@
 function MainCard() {
-    const IMG_EXCLAMATION = props.getProperty("IMG_EXCLAMATION");
+    const IMG_BARS = props.getProperty("IMG_BARS");
     const card = CardService.newCardBuilder();
 
     const header = CardService.newCardHeader()
         .setTitle("<b>TITLE</b>")
         .setSubtitle("subtitle")
         .setImageStyle(CardService.ImageStyle.SQUARE)
-        .setImageUrl(IMG_EXCLAMATION);
+        .setImageUrl(IMG_BARS);
 
     card.setHeader(header);
 
