79a4698ad64d5307207fc93948dd63bee225bcc5
Chris <chrisdavidramos@gmail.com>
2018-09-04 00:57:57 -0400

    bump

--- Code/Service/Location.ts
+++ Code/Service/Location.ts
@@ -7,19 +7,17 @@ function LocationService() {
 
     const data = (new JsonResponseHandler(url, {token: API_IPINFO})).data;
 
-    Logger.log(data);
-
     this.ip = data.ip;
     this.city = data.city;
     this.region = data.region;
     this.coordData = data.loc;
 
-    const lat = Number(coordData.split(",")[0]);
-    const lon = Number(coordData.split(",")[1]);
+    const lat = Number(this.coordData.split(",")[0]);
+    const lon = Number(this.coordData.split(",")[1]);
 
     // weather service api limit precision to 4 decimal places
     this.lat = lat.toFixed(4);
     this.lon = lon.toFixed(4);
 
-    this.coord = String(lat) + "," + String(lon);
+    this.coord = String(this.lat) + "," + String(this.lon);
 }
